10b6d777091d28587fe69d951f9726bb02423315
= <joseph.kingofdreams@gmail.com>
2020-03-16 16:06:46 -0700

    Fixed Slider Bug

--- timer/timer.js
+++ timer/timer.js
@@ -1,12 +1,12 @@
     var timeOut;
 
-    function start(){
+    function timer_start(){
         document.querySelector(".timer").style.visibility = 'visible';
         document.querySelector(".innerTimer").style.display = 'block';
-        timeOut = setTimeout(function(){stop(); }, 10000);
+        timeOut = setTimeout(function(){timer_stop(); }, 10000);
     }
 
-    function stop(){
+    function timer_stop(){
         document.querySelector(".timer").style.visibility = 'hidden';
         document.querySelector(".innerTimer").style.display = 'none';
         timeOut = setTimeout(function(){alert("Out of time!"); }, 1);
